03adfacde074f690db4acf423051089c62598161
Tony Fahrion <tony.wolf@de.clara.net>
2018-05-11 11:25:08 +0200

    Update matomo to 3.5.0 and make the matomo install process easier

--- example/matomo/docker/build.d/deps/302_get_matomo_sources.sh
+++ example/matomo/docker/build.d/deps/302_get_matomo_sources.sh
@@ -3,7 +3,4 @@
 # docs: https://matomo.org/faq/how-to-install/faq_18271/
 
 sectionText "Fetch matomo sources version $MATOMO_VERSION"
-git clone --depth 1 --branch $MATOMO_VERSION --single-branch --config filter.lfs.smudge=true https://github.com/matomo-org/matomo.git /app.source &>> $BUILD_LOG
-rm -rf /app.source/.git
-mv /app.source/* ${WORKDIR}/
-mv /app.source/.[a-z]* ${WORKDIR}/
+curl -L https://github.com/matomo-org/matomo/archive/$MATOMO_VERSION.tar.gz -o - | tar -zxf - --strip 1 &>> $BUILD_LOG
