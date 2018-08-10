0b59766ddcf5c3f517a5b81b05f03c447d36601e
Tony Fahrion <tony.wolf@de.clara.net>
2018-08-10 13:06:56 +0200

    Increase PHP patch level to 7.1.20 and 7.2.8

--- bin/ci.sh
+++ bin/ci.sh
@@ -5,8 +5,8 @@ WORKDIR=$(realpath $0 | xargs dirname | xargs dirname)
 
 export VERSION=$(cat $WORKDIR/VERSION)
 export IMAGE_NAME="claranet/php"
-FROM_IMAGE_TAGS="7.1.18-fpm-jessie 7.2.6-fpm-stretch"
-LATEST_IMAGE="7.2.6-fpm-stretch"
+FROM_IMAGE_TAGS="7.1.20-fpm-jessie 7.2.8-fpm-stretch"
+LATEST_IMAGE="7.2.8-fpm-stretch"
 
 # based on $TRAVIS_BRANCH
 # we decide to...
