f100432919f869bfec8c389c15ab6741e9066150
Tony Fahrion <tony.wolf@de.clara.net>
2018-10-16 14:14:32 +0200

    Update PHP parent image to 7.1.23 and 7.2.11
    7.2.11 is the latest image now

--- bin/image.sh
+++ bin/image.sh
@@ -4,8 +4,8 @@ set -x
 set -eo pipefail
 WORKDIR=$(realpath $0 | xargs dirname | xargs dirname)
 
-export FROM_IMAGE=${FROM_IMAGE:-php:7.2.8-fpm-stretch}
-PHP_VERSION=${PHP_VERSION:-7.2.8}
+export FROM_IMAGE=${FROM_IMAGE:-php:7.2.11-fpm-stretch}
+PHP_VERSION=${PHP_VERSION:-7.2.11}
 VERSION=${VERSION:-`cat $WORKDIR/VERSION`}
 IMAGE_NAME=${IMAGE_NAME:-local/claranet/php}
 IMAGE_TAG=${IMAGE_TAG:-$VERSION-php$PHP_VERSION}
