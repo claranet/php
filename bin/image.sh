f756e359f4e57b48c0d972fa114aaa2a2541e270
Wiktor Sierpinski <wiktor.sierpinski@de.clara.net>
2019-02-05 13:29:12 +0100

    updated 7.1 and 7.2

--- bin/image.sh
+++ bin/image.sh
@@ -4,8 +4,8 @@ set -x
 set -eo pipefail
 WORKDIR=$(realpath $0 | xargs dirname | xargs dirname)
 
-export FROM_IMAGE=${FROM_IMAGE:-php:7.2.11-fpm-stretch}
-PHP_VERSION=${PHP_VERSION:-7.2.11}
+export FROM_IMAGE=${FROM_IMAGE:-php:7.2.14-fpm-stretch}
+PHP_VERSION=${PHP_VERSION:-7.2.14}
 VERSION=${VERSION:-`cat $WORKDIR/VERSION`}
 IMAGE_NAME=${IMAGE_NAME:-local/claranet/php}
 IMAGE_TAG=${IMAGE_TAG:-$VERSION-php$PHP_VERSION}
