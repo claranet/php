5297176350505df1a154688d38df18b0b60a90b4
Hicham Boublahboub <hashbou@users.noreply.github.com>
2020-01-19 21:03:02 +0100

    Update PHP flavour 7.3.7 -> 7.3.8 (#64)

--- bin/image.sh
+++ bin/image.sh
@@ -4,8 +4,8 @@ set -x
 set -eo pipefail
 WORKDIR=$(realpath $0 | xargs dirname | xargs dirname)
 
-export FROM_IMAGE=${FROM_IMAGE:-php:7.3.7-fpm-stretch}
-PHP_VERSION=${PHP_VERSION:-7.3.7}
+export FROM_IMAGE=${FROM_IMAGE:-php:7.3.8-fpm-stretch}
+PHP_VERSION=${PHP_VERSION:-7.3.8}
 VERSION=${VERSION:-`cat $WORKDIR/VERSION`}
 IMAGE_NAME=${IMAGE_NAME:-local/claranet/php}
 IMAGE_TAG=${IMAGE_TAG:-$VERSION-php$PHP_VERSION}
