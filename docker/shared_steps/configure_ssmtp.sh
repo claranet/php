3b45a9c35ca413c4e5e89006b3d814a1f2d8ddc8
Martin Weber <martin.weber@de.clara.net>
2020-05-13 07:57:41 +0200

    Detect ssmtp or msmtp and configure it

--- docker/shared_steps/configure_ssmtp.sh
+++ docker/shared_steps/configure_ssmtp.sh
@@ -14,10 +14,31 @@ FromLineOverride=yes
 EOF
 }
 
+configure_msmtp() {
+    cat > /etc/msmtprc <<EOF
+host ${SMTP_HOST}
+port ${SMTP_PORT}
+
+host mailrelay-exim.services-stage.svc
+port 25
+
+tls off
+tls_starttls off
+
+add_missing_from_header on
+
+EOF
+}
+
 if is_true $ENABLE_SMTP; then
-    sectionText "Configure SSMTP"
-    configure_ssmtp
+    if [ -x "$(command -v ssmtp)" ]; then
+      sectionText "Configure SSMTP"
+      configure_ssmtp
+    fi
+    if [ -x "$(command -v msmtp)" ]; then
+      sectionText "Configure MSMTP"
+      configure_msmtp
+    fi
 else
     sectionText "SKIP: SMTP disabled"
 fi
-
