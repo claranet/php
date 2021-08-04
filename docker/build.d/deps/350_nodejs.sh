a005c3026fe860e588ed9c4b6541a5e1c7e0d4fd
Hicham Boulahboub <hicham.boulahboub@de.clara.net>
2021-08-04 19:42:00 +0200

    Add apt-transport-https to be installed by default

--- docker/build.d/deps/350_nodejs.sh
+++ docker/build.d/deps/350_nodejs.sh
@@ -7,7 +7,13 @@ if ! is_true "$ENABLE_NODEJS"; then
 fi
 
 sectionText "Enable upstream nodejs debian repository"
+# The following line is for allowing to fetch apt-transport-https from buster-updates
+# As the requirement to install apt-transport-https comes from below nodejs bash
+# script we're not able to pin to a specific version of apt-transport-https to avoid this issue.
+sed -i '2,2s/buster/buster*/' /etc/apt/preferences.d/argon2-buster
 eatmydata curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION}.x | eatmydata bash - &>> $BUILD_LOG
+# Undo the above modifiaction to only allow this for this use-case
+sed -i '2,2s/buster*/buster/' /etc/apt/preferences.d/argon2-buster
 update_apt_cache --force
 install_packages --build nodejs npm
 
